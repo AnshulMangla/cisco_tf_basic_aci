e0bb0d33cc13f19421f5b1994e71b70692563020
AnshulMangla <87487619+AnshulMangla@users.noreply.github.com>
2026-08-11 16:25:55 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,9 +9,9 @@ terraform {
 
 # Provider Configuration
 provider "aci" {
-    url = "https://10.106.236.54"
-    username = "pandapa"
-    password = "cisco!123"
+    url = "https://x.x.x.x"
+    username = "anmangla"
+    password = "mangla1234"
 }
 
 resource "aci_tenant" "anshul_terraform5" {
